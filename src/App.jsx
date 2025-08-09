3d517f64c48dc69fc0e286d2dd66f7b19fe9312f
nasii05 <iamnasih@gmail.com>
2025-08-09 21:20:39 +0530

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import FocusInput from "./components/FocusInput.jsx";
-import Timer from "./components/Timer.jsx";
-
+import useFetch from "./hooks/useFetch.jsx";
 
 const App = () => {
+    const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
 
     return (
-      <div>
-          {/*<FocusInput/>*/}
-          <Timer/>
-      </div>
+        <div>
+            {data &&
+                data.map((item) => {
+                    return <p key={item.id}>{item.title}</p>;
+                })
+            }
+        </div>
     )
 }
 export default App
